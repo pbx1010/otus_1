693fb38e65924e33adde6c027deb36d129e7bb39
pbx1010 <itinsideru@gmail.com>
2022-03-24 14:59:34 +0300

    заливка ДЗ1

--- homework_01/main.py
+++ homework_01/main.py
@@ -19,8 +19,37 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+def simple(number):
+    z = 0
+    i = 2
+    while i <= number:
+        if number % i == 0 and i <= number:
+            z += 1
+        i += 1
+    if z <= 1:
+        return number
 
-def filter_numbers():
+
+def filter_numbers(lst, arg):
+    lst_out = []
+    if arg == ODD:
+        while len(lst) >= 1:
+            if lst[0] % 2 != 0 :
+                lst_out.append(lst[0])
+            lst.pop(0)
+    elif arg == EVEN:
+        while len(lst) >= 1:
+            if lst[0] % 2 == 0:
+                lst_out.append(lst[0])
+            lst.pop(0)
+    elif arg == PRIME:
+        while len(lst) >= 1:
+            if simple(lst[0]) == lst[0]:
+                lst_out.append(lst[0])
+            lst.pop(0)
+    return lst_out
+
+
     """
     функция, которая на вход принимает список из целых чисел,
     и возвращает только чётные/нечётные/простые числа
