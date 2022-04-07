22bdc3727a9c6f9335115ac2f216a353bece564b
pbx1010 <itinsideru@gmail.com>
2022-04-07 20:39:17 +0300

    Новый вариант 2 задачи

---
+++ homework_02/__init__.py
@@ -0,0 +1,13 @@
+"""
+Домашнее задание №2
+Классы и модули
+"""
+from . import base_old, car, engine, exceptions, plane
+
+__all__ = [
+    "base_old.py",
+    "car",
+    "engine",
+    "exceptions",
+    "plane",
+]
