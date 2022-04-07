cb8b06b1c1abc6969bd15df9e0ccf63f74749274
pbx1010 <itinsideru@gmail.com>
2022-04-07 18:43:59 +0300

    dz2

---
+++ homework_02/base.py
@@ -0,0 +1,18 @@
+from abc import ABC
+
+
+class Vehicle(ABC):
+
+    def __init__(self, weight=8000, started=True, fuel=400, fuel_consumption=15):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+        self.started = started
+
+    def start(self):
+        if not self.started and self.fuel > 0:
+            self.started = True
+        else:
+            pass
+
+
