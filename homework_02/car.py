d32812951ce7cb2c5abf6ff061e746a763fd227b
pbx1010 <itinsideru@gmail.com>
2022-04-12 18:56:28 +0300

    test_car pass

--- homework_02/car.py
+++ homework_02/car.py
@@ -7,14 +7,13 @@
     объекта `Engine` и устанавливает на текущий экземпляр `Car`
 """
 from homework_02.base import Vehicle
-#from base import Vehicle
+
 
 class Car(Vehicle):
 
-    def __init__(self,engine):
-        self.engine=engine
+    def __init__(self, weight, fuel, fuel_consumption):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.engine = None
 
-    def set_engine(self,engine):
-        self.car=engine
-
-
+    def set_engine(self, engine):
+        self.engine = engine
