2c38663bd1d7ea0a2864175466cf9a42b871a21a
pbx1010 <itinsideru@gmail.com>
2022-04-13 09:41:54 +0300

    plane - test OK

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -15,9 +15,19 @@ from homework_02.exceptions import CargoOverload
 
 class Plane(Vehicle):
 
-    def __init__(self, weight, fuel, fuel_consumption):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
         super().__init__(weight, fuel, fuel_consumption)
-        self.cargo = None
+        self.max_cargo = max_cargo
+        self.cargo = 0
 
-    def load_cargo(self,cargo_weight):
-        CargoOverload
+    def load_cargo(self,cargo):
+        if self.cargo + cargo > self.max_cargo:
+            raise CargoOverload()
+        else:
+            self.cargo += cargo
+
+    def remove_all_cargo(self):
+        z = self.cargo
+        self.cargo = 0
+        return z
+
