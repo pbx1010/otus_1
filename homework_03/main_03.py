77b33a2d6a05b6ee985e9628c922e2ca0157538c
pbx <s8720@rambler.ru>
2022-06-03 23:23:31 +0300

    homework_03

--- homework_03/main_03.py
+++ homework_03/main_03.py
@@ -1,13 +1,47 @@
-from typing import Optional
 from fastapi import FastAPI
+from pydantic import constr
+
+from items_views import router as items_router
+from users.api import router as users_router
 
 app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    # 'hel' "lo" ''' wor''' """ld!"""
+    """
+    ```
+    root.__doc__
+    'hello world!'
+
+    "GET /"
+    ```
+    """
+
+    return {"message": "Hello World!!!"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/hello")
+# def hello_view(name: str = "OTUS"):
+def hello_view(name: constr(min_length=3) = "OTUS"):
+    """
+    GET /hello?name=OTUS
+    :param name:
+    :return:
+    """
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/add")
+def add(a: int, b: int):
+    """
+    :param a:
+    :param b:
+    :return:
+    """
+    return {"a": a, "b": b, "sum": a + b}
+
+
