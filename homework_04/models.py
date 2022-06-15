3dbcba50e9bb13ae23f209cb45434b59a05b15fb
pbx1010 <itinsideru@gmail.com>
2022-06-15 19:52:56 +0300

    homework_04

--- homework_04/models.py
+++ homework_04/models.py
@@ -1,12 +1,10 @@
-# импортируем модуль даты и времени
+
 from datetime import datetime
 
-# импортируем логгер (типа дебаггера)
 from loguru import logger
 
 import asyncio
 
-# импортируем ORM для работы с БД
 from sqlalchemy import (
     Column,
     String,
@@ -16,25 +14,18 @@ from sqlalchemy import (
     func
 )
 
-# импортируем асинхронные методы sqlalchemy для работы с БД
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
-# импортируем метод работы с бд, фабрику сессий и связи
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-
 
 PG_ASYNC_CONN_URI = 'postgresql+asyncpg://username:passwd!@localhost/postgres'
 
-# создаем движок
 engine = create_async_engine(PG_ASYNC_CONN_URI, echo=False)
 
-# создаем метод описания БД (Создаем базовый класс для декларативных определений классов.)
 Base = declarative_base()
 
-# создаем сессию (Фабрика sessionmaker генерирует новые объекты Session при вызове)
 Session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-# запускаем докер, вызвав команду в консоли используя create_subprocess_shell(cmd)
 cmd = 'docker compose up -d'
 
 async def create_pg_docker(cmd):
@@ -42,13 +33,11 @@ async def create_pg_docker(cmd):
      await result.communicate()
      logger.info('____pg docker rdy')
 
-# делаем DROP TABLE, CREATE TABLE в БД
 async def created_db_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
 
-# cоздаем ф-ю, которая скачивает с сайта юзеров и сохраняет их в БД
 async def save_user_in_db(u_data):
     async with Session() as session:
         async with session.begin():
@@ -56,11 +45,9 @@ async def save_user_in_db(u_data):
                 name = user['name']
                 email = user['email']
                 user = User(name=name, email=email)
-                session.add(user)  # добавляем данные юзера в сессию
-                # session.commit() делается автоматически при закрытии менеджера контекста, поэтому его здесь не пишем.
+                session.add(user)
 
 
-# cоздаем ф-ю, которая скачивает с сайта посты и сохраняет их в БД
 async def save_post_in_db(p_data):
     async with Session() as session:
         async with session.begin():
@@ -71,8 +58,6 @@ async def save_post_in_db(p_data):
                 post = Post(title=title, description=description, user_id=user_id)
                 session.add(post)
 
-
-# создаем модель таблицы User и Post
 class User(Base):
     __tablename__ = 'user'
 
