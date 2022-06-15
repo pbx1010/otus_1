fe3dd64aca7562fe0141edf2f017532d7a578c33
pbx1010 <s8720@rambler.ru>
2022-06-15 09:34:04 +0300

    commit

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -6,21 +6,39 @@ import aiohttp
 import asyncio
 
 
-USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users/"
-POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/"
+USERS_DATA_URL = "https://jsonplaceholder.typicode.com/users/1"
+POSTS_DATA_URL = "https://jsonplaceholder.typicode.com/posts/1"
 
 
-async def users():
+async def fetch_json(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(USERS_DATA_URL) as resp:
             print(resp.status)
             print(await resp.text())
+            return await resp.json()
 
-async def posts():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(POSTS_DATA_URL) as resp:
-            print(resp.status)
-            print(await resp.text())
+if __name__ == '__main__':
+    asyncio.run(fetch_json(USERS_DATA_URL))
 
-asyncio.run(users())
-asyncio.run(posts())
+
+
+# async def users():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(USERS_DATA_URL) as resp:
+#             print(resp.status)
+#             #print(await resp.text())
+#             #print(await resp.json())
+#             response_json = (await resp.json())
+#             print(response_json['address'], response_json['website'])
+#             #print(response_json)
+#             #print(type(response_json))
+#
+# async def posts():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(POSTS_DATA_URL) as resp:
+#             print(resp.status)
+#             print(await resp.text())
+#             print(await resp.json())
+#
+# asyncio.run(users())
+# asyncio.run(posts())
