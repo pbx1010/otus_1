7ebd1c446971ec21448236a3d161e349df945f45
pbx1010 <itinsideru@gmail.com>
2022-07-22 14:49:39 +0300

    homework_04

--- homework_04/main.py
+++ homework_04/main.py
@@ -21,10 +21,10 @@ from models import create_pg_docker, created_db_tables, cmd, save_post_in_db, sa
 async def async_main():
 #   await create_pg_docker(cmd)
     await created_db_tables()
-    user_data, post_data = await asyncio.gather(fetch_json(USERS_DATA_URL),
+    users_data, posts_data = await asyncio.gather(fetch_json(USERS_DATA_URL),
                                                 fetch_json(POSTS_DATA_URL))
-    await save_user_in_db(user_data)
-    await save_post_in_db(post_data)
+    await save_user_in_db(users_data)
+    await save_post_in_db(posts_data)
 
 
 def main():
